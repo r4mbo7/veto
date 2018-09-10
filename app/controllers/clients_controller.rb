a3d2217e7eee12c510ae83635eabdfe0dcede501
Constantin DLR <cdlr75@gmail.com>
2018-09-10 11:49:20 +0200

    DEV: Order clients by name

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,7 +4,7 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
-    @clients = Client.all
+    @clients = Client.all.order(:name)
   end
 
   # GET /clients/1
